9bb66a906da988525bf817b09920144f4cbe2531
n/a <n/a>
2016-02-29 21:56:47 +0100

    Initial Commit

---
+++ js/quotes.js
@@ -0,0 +1,5 @@
+var quotes = ["I know words, I have the best words", "Everyone loves me"];
+var quote = quotes[Math.floor(Math.random()*quotes.length)];
+
+document.getElementById("quote").innerHTML = quote;
+ 
